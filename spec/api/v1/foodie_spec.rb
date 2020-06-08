5b5728a071c19ae80723a585ca236192fc444ebc
Ethan Hocking <eshocking253@gmail.com>
2020-06-08 11:58:32 -0600

    Add spec for json attributes

--- spec/api/v1/foodie_spec.rb
+++ spec/api/v1/foodie_spec.rb
@@ -7,6 +7,18 @@ describe 'get foodie route', type: :request do
   end
 
   it 'returns proper attributes' do
+    get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
+    expected_attributes = [
+      "end_location",
+      "travel_time",
+      "forecast",
+      "resturant"
+     ]
+    json = JSON.parse(response.body)
+    expect(json["data"]["attributes"].keys).to match_array expected_attributes
+  end
 
+  it 'returns correct data' do
+    
   end
 end
